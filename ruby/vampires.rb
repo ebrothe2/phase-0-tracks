c8313860933a4e5313a314d1d6f5bc0d928dd1ca
ebrothe2 <eholmes3@gmail.com>
2016-05-26 21:40:22 -0400

    added initial vampire questions to gather data

---
+++ ruby/vampires.rb
@@ -0,0 +1,8 @@
+puts "What is your name?"
+  name = gets.chomp
+puts "How old are you?"
+  age = gets.chomp
+puts "What year were you born?"
+  year = gets.chomp
+puts "Would you like some garlic bread? (y/n)"
+puts "Would you like to enroll for Health Insurance? (y/n)"
