ddb877f0ed3db4367a2ab74eaea641023863a075
ebrothe2 <eholmes3@gmail.com>
2016-05-28 22:40:50 -0400

    added method and employee counter

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,47 +1,62 @@
-puts "What is your name?"
-  name = gets.chomp
-puts "How old are you?"
-  age = gets.chomp.to_i
-puts "What year were you born?"
-  year_born = gets.chomp.to_i
-  current_year = 2016
-      if (current_year - year_born) == age
-        age = true
-        else
-        age = false
-        end
+def decide_if_new_employee_is_vampire
+  puts "What is your name?"
+    name = gets.chomp
+  puts "How old are you?"
+    age = gets.chomp.to_i
+  puts "What year were you born?"
+    year_born = gets.chomp.to_i
+    current_year = 2016
+        if (current_year - year_born) == age
+          age = true
+          else
+          age = false
+          end
 
 
-puts "Would you like some garlic bread? (y/n)"
-  eats_garlic_bread = gets.chomp
-  if eats_garlic_bread == "y"
-    eats_garlic_bread = true
-  elsif eats_garlic_bread == "n"
-    eats_garlic_bread = false
-  end
-
-puts "Would you like to enroll for Health Insurance? (y/n)"
-  insurance_sign_up = gets.chomp
-    if insurance_sign_up == "y"
-      insurance_sign_up = true
-    elsif insurance_sign_up == "n"
-      insurance_sign_up = false
+  puts "Would you like some garlic bread? (y/n)"
+    eats_garlic_bread = gets.chomp
+    if eats_garlic_bread == "y"
+      eats_garlic_bread = true
+    elsif eats_garlic_bread == "n"
+      eats_garlic_bread = false
     end
 
-#write an if statement meeting these conditions and if meets none of the criteria results are inconclusive
-#if age == true && (eats_garlic_bread == true || insurance_sign_up = true) then "probably not a vampire"
-#if age = false && (eats_garlic_bread = false || insurance_sign_up = false) then probably a vampire
-#if age = false && eats_garlic_bread = false && insurance_sign_up = false then "almost certainly a vampire"
-#if name == "Drake Cula" || "Tu Fang" then "definitely a vampire"
+  puts "Would you like to enroll for Health Insurance? (y/n)"
+    insurance_sign_up = gets.chomp
+      if insurance_sign_up == "y"
+        insurance_sign_up = true
+      elsif insurance_sign_up == "n"
+        insurance_sign_up = false
+      end
 
-  if (name == "drake cula") || (name == "Tu Fang")
-    puts "Definitely a vampire"
-  elsif !age && !eats_garlic_bread && !insurance_sign_up
-    puts "almost certainly a vampire"
-  elsif !age && (!eats_garlic_bread || !insurance_sign_up)
-    puts "Probably a vampire"
-  elsif age && (eats_garlic_bread || insurance_sign_up)
-    puts "probably not a vampire"
-  else
-    puts "results inconclusive"
-  end
+  #write an if statement meeting these conditions and if meets none of the criteria results are inconclusive
+  #if age == true && (eats_garlic_bread == true || insurance_sign_up = true) then "probably not a vampire"
+  #if age = false && (eats_garlic_bread = false || insurance_sign_up = false) then probably a vampire
+  #if age = false && eats_garlic_bread = false && insurance_sign_up = false then "almost certainly a vampire"
+  #if name == "Drake Cula" || "Tu Fang" then "definitely a vampire"
+
+    if (name == "drake cula") || (name == "Tu Fang")
+      puts "Definitely a vampire"
+    elsif !age && !eats_garlic_bread && !insurance_sign_up
+      puts "almost certainly a vampire"
+    elsif !age && (!eats_garlic_bread || !insurance_sign_up)
+      puts "Probably a vampire"
+    elsif age && (eats_garlic_bread || insurance_sign_up)
+      puts "probably not a vampire"
+    else
+      puts "results inconclusive"
+    end
+
+end
+#decide_if_new_employee_is_vampire
+
+puts "How many new employees need to be surveyed?"
+  new_employees_number = gets.chomp.to_i
+
+counter = 0
+
+until counter == new_employees_number
+  decide_if_new_employee_is_vampire
+  counter += 1
+end
+
