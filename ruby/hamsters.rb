c225d4da82938f4a98d68ffafcdc4f1917fc3086
ebrothe2 <eholmes3@gmail.com>
2016-05-30 13:48:39 -0400

    adjusting pseudocode notes

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -6,19 +6,10 @@
 #AGE:Ask for estimated age
 #AGE-NIL: if estimated age is blank assign nil
 
-#define the variables
-hamster_name = #{name}
-squeak_volume = #{volume}
-fur_color = #{color}
-adoptive_candidate = #{adoption}
-estimated_age = #{age}
-
-#define the method
-#INTRODUCTION: introduce the Hamster Haven & what we need to help the hamster
+#gathering data from user on their hamster
 puts "Welcome to Hamster Haven! \r\nThis is the place where hamsters are saved, \r\nhave an opportunity to be rejuvenated, \r\n and then\r\nare adopted into caring homes."
 puts "\r\nWe would like to ask you some questions about your hamster."
 
-#NAME: define method for name
 valid_input = false
 until valid_input
 puts "\r\nFirst, all our hamsters are referred to by name."
@@ -38,7 +29,6 @@ puts "Have you given your hamster a name? (y/n)"
   end
 end
 
-#VOLUME: define method for volume
 puts "\r\nWe want to make sure we match a talkative hamster to someone who enjoys listening to their hamster's banter."
 puts "\r\nTherefore, we need to know how talkative (some people might call it noisy) your hamster is."
 
@@ -46,11 +36,9 @@ puts "On a scale of 1-10; 1 being extremely quiet, and 10 being extremely noisy,
   squeak_volume = gets.chomp
   squeak_volume = squeak_volume.to_i
 
-#COLOR: define method for fur color
 puts "What color is the hamster's fur?"
   fur_color = gets.chomp
 
-#ADOPT: define method for whether of not hamster is good adoptive candidate
 valid_input = false
 until valid_input
 puts "Is your hamster a good candidate for adoption? (y/n)"
@@ -67,9 +55,8 @@ puts "Is your hamster a good candidate for adoption? (y/n)"
   end
 end
 
-#AGE: define method for asking age
 #estimated age returns a string
-#try method to see if string is empty
+#try to see if string is empty
 #if true and string is empty then puts n/a and assign nil
 #if false and string is not empty try then setting estimated age to in integer?
 puts "What is the estimated age of hamster?"
@@ -80,13 +67,10 @@ puts "What is the estimated age of hamster?"
     estimated_age = estimated_age.to_i
   end
 
-#refine the code using the general format in lines 17-36
 
 puts "Thank you for saving your hamster and bringing them to the sanctuary!"
 puts  "\r\nHere is a summary of #{hamster_name}'s information:"
 
-
-#call the method
 puts "Hamster's name is #{hamster_name}"
 puts "Hamster's squeak_volume is #{squeak_volume}"
 puts "Hamster's fur color is #{fur_color}"
