e271da319605b78234f7b16c9e4f4d7367c905f7
ebrothe2 <eholmes3@gmail.com>
2016-06-04 14:37:09 -0400

    added release 2 part of assignment from pairing

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,14 @@
+
+def build_array(x, y, z)
+new_array = [x, y, z]
+p new_array
+end
+
+def add_to_array(array, item)
+new_array = array.push(item)
+p new_array
+end
+
 array = []
 p array
 array = ["1","2","3","4","5"]
@@ -9,11 +20,15 @@ p array
 array.shift
 p array
 if array.include?("100") == true
-  puts "The array includes 100."
+puts "The array includes 100."
 else
-  puts "The array does not include 100."
+puts "The array does not include 100."
 end
 
 array2 = ["one","two","three","four"]
 array3 = array + array2
 p array3
+
+build_array("first", "second", "third")
+add_to_array(["one", "two", "three"], "4")
+
