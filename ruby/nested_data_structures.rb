a7442f040af6d7fab51caab11772b734d2177253
ebrothe2 <eholmes3@gmail.com>
2016-06-05 02:46:24 -0400

    adding nested access and pairing

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -2,38 +2,54 @@
 #grocery store
 
 grocery_store = {
-  produce_section:{
-    fruits:{
-      berries: [
+  'produce_section' => {
+    'fruits'=> {
+      'berries' => [
         "strawberries","blueberries","raspberries","blackberries"
-      ]
-      apples: [
+      ],
+      'apples'=> [
         "fuji","braeburn","golden_delicious","jonathon"
-      ]
-      citrus:[
+      ],
+      'citrus'=> [
         "grapefruit","lemon","lime","orange"
-      ]
-    }
-    vegetables:{
-      lettuce:[
+      ],
+    },
+    'vegetables' => {
+      'lettuce'=> [
         "spinach","kale","romaine","arugula"
-      ]
-      squash:[
+      ],
+      'squash'=> [
         "zucchini","butternut","yellow squash","spaghetti squash"
-      ]
-
-    }
-  }
-  bakery:{
-
-  }
-  seafood:{
-
-  }
-  meat:{
-
-  }
-  frozen_foods:{
-
-  }
+      ],
+    },
+  },
+  'bakery' => {
+    'cakes'=> [
+      "chocolate", "vanilla", "marble"
+    ],
+    'pies'=> [
+      "apple","cherry","pumpkin"
+    ],
+  },
+  'meat' => {
+    'beef_cuts'=> [
+      "flank steak","ribeye","ground chuck"
+    ],
+    'poultry'=> [
+      "turkey breast","chicken legs","whole chicken"
+    ],
+  },
+  'frozen_foods' => {
+    'desserts'=> [
+      "ice cream","frozen pies",
+    ],
+    'vegetables'=> [
+      "carrots", "broccoli", "mixed vegetables"
+    ],
+  },
 }
+
+p grocery_store['produce_section']['fruits']['berries'][0]
+p grocery_store['bakery']['cakes'][2]
+grocery_store['bakery']['cakes'].push('strawberry')
+p grocery_store['bakery']['cakes']
