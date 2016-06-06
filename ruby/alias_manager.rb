537275392e9c5678e3f75036f5863b5f5b5ff783
ebrothe2 <eholmes3@gmail.com>
2016-06-05 21:03:30 -0400

    adding looping through alias name

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,30 @@
+#writing a method for creating a fake name from a real name
+#input is a first and last name
+#first divide the first and last name into seperate strings and switch their order
+
+puts "What is your name? (first and last)"
+name = gets.chomp.split(' ')
+name.rotate!
+
+name[0].downcase!
+name[1].downcase!
+
+#now we have an array of two strings assigned to name
+#split name into individual letters
+name[0] = name[0].split('')
+name[1] = name[1].split('')
+
+#p name
+
+#now there is an array full of strings, each individual letter of the new first name
+#need to look at each letter, if consonant go to next consonant and if vowel go to next vowel
+
+name.map!{|letter| letter.next}
+
+#p name
+
+new_first_name = name[0].join('')
+new_last_name = name[1].join
+p new_first_name.capitalize
+p new_last_name.capitalize
+
