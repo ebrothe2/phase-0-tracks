cb0beca756bfe86f793bd2856d49d557dbad9d9f
ebrothe2 <eholmes3@gmail.com>
2016-06-10 19:42:25 -0400

    gave santas attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -15,20 +15,35 @@ class Santa
     puts "That was a good #{cookie_type}!"
   end
 
+  def celebrate_birthday
+    @age = @age.to_i + 1
+  end
+
+  def age
+    @age
+  end
+
+  def get_mad_at
+    p @reindeer_ranking
+  end
+
 end
 
-santas_list = []
-santas_list << Santa.new('female', 'black')
-santas_list << Santa.new('male', 'Latino')
-santa_genders = ["undecided", "transgender", "female", "male"]
-santa_ethnicities = ['Polish', 'Moroccan', 'Mexican', 'Brazilian']
+# santas_list = []
+# santas_list << Santa.new('female', 'black')
+# santas_list << Santa.new('male', 'Latino')
+# santa_genders = ["undecided", "transgender", "female", "male"]
+# santa_ethnicities = ['Polish', 'Moroccan', 'Mexican', 'Brazilian']
 
-santa_genders.length.times do |each|
-  santas_list << Santa.new(santa_genders[each], santa_ethnicities[each])
-end
+# santa_genders.length.times do |each|
+#   santas_list << Santa.new(santa_genders[each], santa_ethnicities[each])
+# end
 
-p santas_list
+# p santas_list
 
 santa = Santa.new('male','white')
+santa.celebrate_birthday
+puts "Santa's age is #{santa.age}"
+santa.get_mad_at
 santa.speak
 santa.eat_milk_and_cookies('oreo')
