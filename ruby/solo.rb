d40cbf6ef32c31b96af35a495801eee1d4640377
ebrothe2 <eholmes3@gmail.com>
2016-06-11 12:43:08 -0400

    pseudocode for designing a class

---
+++ ruby/solo.rb
@@ -0,0 +1,11 @@
+#Design a class
+#designing a class for a toddler
+#3 attributes a toddler can have;
+#a toddler can have an age of 1-3
+#a toddler has a weight
+#a toddler has an eye color
+
+#3 methods a toddler needs
+#a toddler plays
+#a toddler can lose their mind aka have a temper tantrum
+#a toddler can, if you're lucky, take a nap
