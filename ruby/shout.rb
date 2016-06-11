5365045b02f28a25ac635c0c4586e57694a498f0
ebrothe2 <eholmes3@gmail.com>
2016-06-11 16:05:38 -0400

    converted standalone shout module to mixin module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -11,5 +11,29 @@
 # p Shout.yell_angrily("Darn it")
 # p Shout.yelling_happily("Oh Great")
 
+module Shout
+  def yell_angrily(words)
+    words + '!!!!' + ' :('
+  end
 
+  def yelling_happily(words)
+    words + '!!!!!!!!' + ' :) :) :)'
+  end
+end
+
+class Referee
+ include Shout
+end
+
+class Coach
+  include Shout
+end
+
+referee = Referee.new
+p referee.yell_angrily('Red Flag')
+p referee.yelling_happily('Goal')
+
+coach = Coach.new
+p coach.yell_angrily('Sit down')
+p coach.yelling_happily('We Won')
 
