94cb96e0c0d2198637a182c64b5e059ddf53ba41
ebrothe2 <eholmes3@gmail.com>
2016-06-15 22:54:39 -0500

    added pseudocode for reverse function

---
+++ js/explore.js
@@ -0,0 +1,17 @@
+//writing a function to reverse a string
+//first need to give a string to reverse
+//then look at the string and divide it into characters
+//number each of the characters
+//reverse the number of the characters
+// then join the characters back together again
+
+// var str = "Hello";
+
+// ended up being able to call .reverse instead of reversing the index since it returns an array.
+function reversestring(str) {
+  console.log(str.split('').reverse().join(''));
+}
+
+var reverse = reversestring("Hello")
+
+if (1=1) { console.log(reverse)}
